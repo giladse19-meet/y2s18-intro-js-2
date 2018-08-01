e70ee255cd2f63cd8729c965989e8b560bb246c4
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:14:26 +0300

    done

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,10 @@
 // Create your 'me' object literal here!
+var me = {
+	firstName: 'Gilad',
+	lastName: 'Segev',
+	favoriteFoods: ['pasta', 'pizza', 'bread'],
+	age: 16,
+	picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png'
+}
+var me_str = JSON.stringify(me);
+document.write(me_str);
