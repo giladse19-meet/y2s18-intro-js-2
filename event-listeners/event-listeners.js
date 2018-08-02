99aa56171448e9a6ee0bf6a8ab0292c848117e73
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 16:36:23 +0300

    even better

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -4,11 +4,12 @@ var currentIndex = 0;
 var slide = document.getElementById('picture');
 
 
-function showNextPicture() {
-  currentIndex++; // increment current picture
-  // if currentIndex is too large, start from the beginning again
-  if (currentIndex >= pictures.length) {
-    currentIndex = 0;
-  }
-  slide.src = pictures[currentIndex]
-}
+slide.addEventListener('click',function showNextPicture() {
+	  currentIndex++; // increment current picture
+	  // if currentIndex is too large, start from the beginning again
+	  if (currentIndex >= pictures.length) {
+	    currentIndex = 0;
+	  }
+	  slide.src = pictures[currentIndex]
+});
+	
